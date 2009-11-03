4c7ff1d267e07a62ecf5aabedac84fc0e59ee486
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2009-11-03 13:50:07 +0000

    add the missing test files

---
+++ trunk/meshdeformer/test/testLog.h
@@ -0,0 +1,18 @@
+#include "Log.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(LogTest,testInit)
+{
+  Log * log = Log::getInstance();
+}
+
+TEST(LogTest,testLogString)
+{
+  Log::getInstance()->logString("testLogString");
+}
+
+TEST(LogTest,testPrintf)
+{
+}
+
