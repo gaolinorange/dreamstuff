790a5d68212b76314ee864ec756916f88458668f
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2009-11-09 14:17:32 +0000

    modified the glheaders and change CMakeLists.txt for apple compiling

--- trunk/meshdeformer/Common/glheaders.hpp
+++ trunk/meshdeformer/Common/glheaders.hpp
@@ -5,15 +5,17 @@
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #include <GLUT/glut.h>
+#endif
 
-#elseif __WIN32__
+#ifdef __WIN32__
 //for windows
 #include <windows.h>
 #include <gl/gl.h>
 #include <gl/glu.h>
 #include <gl/glut.h>
+#endif
 
-#else  //for linux
+#ifdef __LINUX__  //for linux
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
