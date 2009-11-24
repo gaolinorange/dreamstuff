74e377712a1801636e686da670ffb64d8d228332
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2009-11-24 14:20:00 +0000

    django learning upadte

--- djangoLearning/mysite/urls.py
+++ djangoLearning/mysite/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from mysite.views import hello,current_datetime,my_homepage_view
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,6 +9,11 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
     # (r'^mysite/', include('mysite.foo.urls')),
+    ('^hello/$',hello),
+    ('^time/$',current_datetime),		       
+    #for site root
+    ('^$',my_homepage_view),       
+		       
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
