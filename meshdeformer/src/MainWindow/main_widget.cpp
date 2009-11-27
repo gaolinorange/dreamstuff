17fc5705b5ac5f66ee67e1f6dc8a72d8b6ff6813
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2009-11-27 08:40:53 +0000

    Log.hpp instance ignored

--- meshdeformer/src/MainWindow/main_widget.cpp
+++ meshdeformer/src/MainWindow/main_widget.cpp
@@ -1,19 +1,44 @@
 #include "main_widget.h"
+#include "Viewport.hpp"
+#include "Camera.hpp"
+#include "Arcball.hpp"
 
 void MainWidget::initializeGL()
 {
   qglClearColor(Qt::black);
   glShadeModel(GL_FLAT);
   glEnable(GL_DEPTH_TEST);
-  glEnable(GL_CULL_FACE);  
-  
+  glEnable(GL_CULL_FACE); 
+
+  //setup render related class
+  m_pViewport = new CViewport();
+  m_pCamera = new CCamera();
+  m_pCamera->SetPosition(CVector3d(0,0,20));
+  m_pCamera->SetOrientation(0,0,-1,0);
+  m_pCamera->SetHeightAngle(PI*0.5);
+  m_pCamera->SetNearDistance(0.1f);
+  m_pCamera->SetFarDistance(100.0f);
+
+  m_pArcball = new CArcball();
+  m_pArcball->SetCenter(CVector3d(0,0,0));
+  m_pArcball->SetRadius(10.0f);  
 }
 
 void MainWidget::resizeGL(int w,int h)
 {
-  glViewport(0,0,(GLint)w,(GLint)h);
+/*
+	glViewport(0,0,(GLint)w,(GLint)h);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
+*/
+	if(m_pViewport)
+	{
+		m_pViewport->SetSize(w,h);
+	}
+	else
+	{
+		m_pViewport = new CViewport(w,h);
+	}
 
   //todo
 
