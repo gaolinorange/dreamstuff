bc309817bd8b210e205049eb2b1a866e0a0c9d25
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2009-11-28 01:56:44 +0000

    by checking the tester generated tetrahedron.off file, I did not define the position of tetrahedron vertices, so it could not display anything

--- trunk/meshdeformer/src/MeshCore/MeshCore.cpp
+++ trunk/meshdeformer/src/MeshCore/MeshCore.cpp
@@ -1,9 +1,6 @@
-//#include <OpenGL/opengl.h>
-
-
-
-
+#include "glheaders.hpp"
 #include "MeshCore.h"
+#include <QtDebug>
 
 MeshCore::MeshCore()
 {
@@ -15,7 +12,7 @@ MeshCore::~MeshCore()
 
 void MeshCore::render()
 {
-  /* 
+	/*
   Vertex_handle v0,v1;
   for(Halfedge_iterator pHalfedge = halfedges_begin();
       pHalfedge != halfedges_end(); pHalfedge++)
@@ -26,6 +23,28 @@ void MeshCore::render()
       glVertex3f(v0->point().x(),v0->point().y(),v0->point().z());
       glVertex3f(v1->point().x(),v1->point().y(),v1->point().z());
       glEnd();
-    }
-  */
+    }*/
+	Halfedge_around_facet_circulator pHalfedge;
+	//Vertex_handle pVertex;
+	qDebug()<<"begin mesh";
+	for(Facet_iterator pFacet = facets_begin();
+		pFacet != facets_end(); pFacet++){
+			glBegin(GL_POLYGON);
+			pHalfedge = pFacet->facet_begin();
+			qDebug()<<"begin facet";
+			do{
+	//			pVertex = pHalfedge->vertex();
+	/*			glVertex3f(pVertex->point().x(),
+					pVertex->point().y(),
+					pVertex->point().z());
+					*/
+				Point_3 point = pHalfedge->vertex()->point();
+				glVertex3f(point[0],point[1],point[2]);
+				qDebug()<<point[0]<<","<<point[1]<<","<<point[2];
+//				qDebug()<<pVertex->point().x()<<","<<pVertex->point().y()<<","<<pVertex->point().z();
+			}while(++pHalfedge != pFacet->facet_begin());
+			glEnd();
+			qDebug()<<"end facet";
+	}
+	qDebug()<<"end mesh";
 }
