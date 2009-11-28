906574efd86f27fe5d78dd9f017fd3b4f83e579a
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2009-11-28 11:40:47 +0000

    add assimp loader ,not tested

--- trunk/meshdeformer/src/MainWindow/app_data.h
+++ trunk/meshdeformer/src/MainWindow/app_data.h
@@ -5,7 +5,13 @@
 #define APPDATA_H
 
 #include <QtGui>
+/*
+#include "assimp.hpp"
 
+typedef class Assimp::Importer Importer;
+typedef class Assimp::aiScene aiScene;
+typedef class Assimp::aiMesh aiMesh;
+*/
 
 class MeshCore;
 
@@ -18,7 +24,7 @@ class AppData //: public QObject
   ~AppData();
  public:
   void initATetrahedron();
-  void load(const QString& filename);
+  bool load(const QString& filename);
   void update();
   void render();
  private:
--- trunk/meshdeformer/src/MeshCore/MeshCore.cpp
+++ trunk/meshdeformer/src/MeshCore/MeshCore.cpp
@@ -26,25 +26,15 @@ void MeshCore::render()
     }*/
 	Halfedge_around_facet_circulator pHalfedge;
 	//Vertex_handle pVertex;
-	qDebug()<<"begin mesh";
 	for(Facet_iterator pFacet = facets_begin();
 		pFacet != facets_end(); pFacet++){
 			glBegin(GL_POLYGON);
 			pHalfedge = pFacet->facet_begin();
-			qDebug()<<"begin facet";
 			do{
-	//			pVertex = pHalfedge->vertex();
-	/*			glVertex3f(pVertex->point().x(),
-					pVertex->point().y(),
-					pVertex->point().z());
-					*/
 				Point_3 point = pHalfedge->vertex()->point();
 				glVertex3f(point[0],point[1],point[2]);
 				qDebug()<<point[0]<<","<<point[1]<<","<<point[2];
-//				qDebug()<<pVertex->point().x()<<","<<pVertex->point().y()<<","<<pVertex->point().z();
 			}while(++pHalfedge != pFacet->facet_begin());
 			glEnd();
-			qDebug()<<"end facet";
 	}
-	qDebug()<<"end mesh";
 }
