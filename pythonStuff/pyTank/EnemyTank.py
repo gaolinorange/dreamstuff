eb6fe1a1b7a6402136227a4d042cb15c3c2bdc5a
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2010-01-27 13:58:35 +0000

    add enemytank render_area

--- pythonStuff/pyTank/EnemyTank.py
+++ pythonStuff/pyTank/EnemyTank.py
@@ -9,21 +9,49 @@ class EnemyTank(pygame.sprite.Sprite):
         self.image,self.rect = load_image('enemytank.png',-1)
         self.screen = screen
         self.pos = Vector2(100,100)
+	self.element_width = 32
+	self.element_height = 32
+	self.render_area = ((0,0),(self.element_width,self.element_height))
+	self.render_swap_flag = True
 
     def move_up(self):
         self.pos += Vector2(0,-1)
+	if self.render_swap_flag == True:
+	    self.render_area = ((0,0),(self.element_width,self.element_height))
+	    self.render_swap_flag = False
+	else:
+	    self.render_area = ((self.element_width,0),(self.element_width,self.element_height))
+	    self.render_swap_flag = True
 
     def move_down(self):
         self.pos += Vector2(0,1)
+	if self.render_swap_flag == True:
+	    self.render_area = ((self.element_width*6,0),(self.element_width,self.element_height))
+	    self.render_swap_flag = False
+	else:
+	    self.render_area = ((self.element_width*7,0),(self.element_width,self.element_height))
+	    self.render_swap_flag = True
 
     def move_left(self):
         self.pos += Vector2(-1,0)
+	if self.render_swap_flag == True:
+	    self.render_area = ((self.element_width*2,0),(self.element_width,self.element_height))
+	    self.render_swap_flag = False
+	else:
+	    self.render_area = ((self.element_width*3,0),(self.element_width,self.element_height))
+	    self.render_swap_flag = True
 
     def move_right(self):
         self.pos += Vector2(1,0)
+	if self.render_swap_flag == True:
+	    self.render_area = ((self.element_width*4,0),(self.element_width,self.element_height))
+	    self.render_swap_flag = False
+	else:
+	    self.render_area = ((self.element_width*5,0),(self.element_width,self.element_height))
+	    self.render_swap_flag = True
 
     def render(self):
-        self.screen.blit(self.image,(self.pos.x,self.pos.y))
+        self.screen.blit(self.image,(self.pos.x,self.pos.y),self.render_area)
 
     def fire(self):
         print "todo: enemy fire"
