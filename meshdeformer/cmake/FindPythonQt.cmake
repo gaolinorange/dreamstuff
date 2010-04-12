cb3d796580a69abb2a517955e153d43b37189b8d
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2010-04-12 06:26:10 +0000

    update cmake script for pythonQt

--- meshdeformer/cmake/FindPythonQt.cmake
+++ meshdeformer/cmake/FindPythonQt.cmake
@@ -1,13 +1,20 @@
-find_path(PYTHONQT_INCLUDE_DIR PythonQt.h /usr/include/PythonQt /usr/local/include/PythonQt)
+find_path(PYTHONQT_INCLUDE_DIR
+  NAME PythonQt.h
+  PATHS /usr/include/PythonQt /usr/local/include/PythonQt
+  )
 
-find_library(PYTHONQT_LIBRARY NAMES PythonQt PATH /usr/lib /usr/local/lib)
+find_library(PYTHONQT_LIBRARY
+  NAMES PythonQt
+  PATH /usr/lib /usr/local/lib
+  )
 
 if(PYTHONQT_INCLUDE_DIR AND PYTHONQT_LIBRARY)
   set(PythonQt_FOUND TRUE)
+  message(STATUS "Found PythonQt Include Directory: ${PYTHONQT_INCLUDE_DIR}")
+  message(STATUS "Found PythonQt Library: ${PYTHONQT_LIBRARY}")
+else(PYTHONQT_INCLUDE_DIR AND PYTHONQT_LIBRARY)
+  message(FATAL_ERROR "Could not find PythonQt")
+  set(PythonQt_FOUND FALSE)
 endif(PYTHONQT_INCLUDE_DIR AND PYTHONQT_LIBRARY)
 
-if(PythonQt_FOUND)
-  message(STATUS "Found PythonQt: ${PYTHONQT_LIBRARY}")
-else(PythonQt_FOUND)
-  message(FATAL_ERROR "Could not find PythonQt")
-endif(PythonQt_FOUND)
+
