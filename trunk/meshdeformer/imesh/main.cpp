2946e230858a0f23204f759a3cfe216a3846e0fd
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2010-04-23 02:24:32 +0000

    add google-glog for logging and add some relevant scripts

--- trunk/meshdeformer/imesh/main.cpp
+++ trunk/meshdeformer/imesh/main.cpp
@@ -2,15 +2,21 @@
 #include "Core/MainWindow.h"
 #include "Common/globals.hpp"
 
+//Google logging library, see 3rdparty/glog_doc for documentation
+#include <glog/logging.h>
+
 bool RUNNING_AT_TEST_MODE = false;
 
 int main(int argc,char ** argv)
 {
   QApplication app(argc,argv);
+  google::InitGoogleLogging( argv[ 0 ] );
+LOG( INFO ) <<"====== Begin Loggiing ======";
 
   MainWindow * mainWindow = new MainWindow();
   mainWindow->resize(800,600);
   mainWindow->showMaximized();
 
+LOG( INFO ) <<"====== End Logging =======";
   return app.exec();
 }
