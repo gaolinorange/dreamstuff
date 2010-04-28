4a87916098980803e183902bcca9a33daff10170
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2010-04-28 02:36:51 +0000

    add learning notes about gae and py2exe

--- trunk/meshdeformer/test/testCurvature.h
+++ trunk/meshdeformer/test/testCurvature.h
@@ -16,6 +16,8 @@
 #define _TESTCURVATURE_H_
 
 #include <gtest/gtest.h>
+#include <cmath>
+
 #include "MeshCore/MeshCore.h"
 #include "Plugin-Curvature/Curvature.h"
 
@@ -23,25 +25,35 @@ class CurvatureTest : public ::testing::Test
 {
  protected:
   virtual void SetUp(  ) {
-    pMesh = new MeshCore(  );
-    Halfedge_handle h = pMesh->make_tetrahedron( Point_3( 0,0,0 ),
+    mesh_ = new MeshCore(  );
+    Halfedge_handle h = mesh_->make_tetrahedron( Point_3( 0,0,0 ),
                                                  Point_3( 0,0,1 ),
                                                  Point_3( 0,1,0 ),
                                                  Point_3( 1,0,0 )
                                                  );
-    pMesh->set_indices( );
-    pMesh->init_index_to_vertex_map(  );
+    mesh_->set_indices( );
+    mesh_->init_index_to_vertex_map(  );
 
     curvature_ = new Curvature(  );
-    curvature_->setMesh( pMesh );
+    curvature_->setMesh( mesh_ );
+
+    cot45Value = 1.0/tan( ( 45.0/180.0 ) * CGAL_PI );
+    cot90Value = 1.0/tan( 0.5 * CGAL_PI );
+    epislon = 1e-6;
   }
+  
   virtual void TearDown(  ) {
-    delete pMesh;
+    delete mesh_;
     delete curvature_;
   }
 
-  MeshCore* pMesh;
+  MeshCore* mesh_;
   Curvature * curvature_;
+
+protected: //precalculated data
+  float cot45Value;
+  float cot90Value;
+  float epislon;
 };
 
 TEST_F( CurvatureTest, testInit ) {
@@ -51,6 +63,16 @@ TEST_F( CurvatureTest, testInit ) {
 TEST_F( CurvatureTest, testEdgeCotValues ) {
   curvature_->calculateEdgeCotValues(  );
   //TODO: add some assertions
+  for (Halfedge_iterator pHalfedge = mesh_->halfedges_begin( );
+       pHalfedge != mesh_->halfedges_end( ); pHalfedge++)
+  {
+    Vertex_handle v0 = pHalfedge->vertex(  );
+    Vertex_handle v1 = pHalfedge->opposite( )->vertex(  );
+    Vector_3 vec = v1->point(  )-v0->point(  );
+
+    
+  }
+  
   
 }
 
