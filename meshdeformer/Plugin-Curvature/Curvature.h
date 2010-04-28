b4f0564865d7ec116b05717356fd1ad2159eb3d0
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2010-04-28 06:30:04 +0000

    add some test for curvature(TODO: how to avoid segment fault in linux

--- meshdeformer/Plugin-Curvature/Curvature.h
+++ meshdeformer/Plugin-Curvature/Curvature.h
@@ -106,24 +106,64 @@ public:
   float getHalfedgeCotValue( Halfedge_handle pHalfedge ) {
     return get( edgeCotValues,pHalfedge );
   }
+private:
+  /**\brief: calculate the angle between between two vector
+     v1v0,v2v1
+     @param: v0, the first point
+     @param: v1, the second point( related with the angle )
+     @param: v2, the third point
+  */
+  float _calculateAngle( Vertex_handle v0, Vertex_handle v1, Vertex_handle v2 ) {
+    Vector_3 v0v1, v1v2, v2v0;
+    float length_v0v1, length_v1v2, length_v2v0;
+    //calculate <v0v1v2
+    //a^2 = b^2+c^2-2*b*c*cos( alpha )
+    //so: v2v0^2 = v0v1^2+v1v2^2-2*v0v1*v1v2*cos( alpha )
+    v0v1 = v1->point(  )-v0->point(  );
+    v1v2 = v2->point(  )-v1->point(  );
+    v2v0 = v0->point(  )-v2->point(  );
+    // b^2
+    length_v0v1 = ( v0v1.x(  )*v0v1.x(  ) +
+                        v0v1.y(  )*v0v1.y(  ) +
+                        v0v1.z(  )*v0v1.z(  ));
+    //c^2
+    length_v1v2 = ( v1v2.x(  )*v1v2.x(  ) +
+                        v1v2.y(  )*v1v2.y(  ) +
+                        v1v2.z(  )*v1v2.z(  ));
+    //a^2
+    length_v2v0 = ( v2v0.x(  )*v2v0.x(  ) +
+                        v2v0.y(  )*v2v0.y(  ) +
+                        v2v0.z(  )*v2v0.z(  ));
+    //b
+    float length_v0v1_sqrt = sqrt( length_v0v1 );
+    //c
+    float length_v1v2_sqrt = sqrt( length_v1v2 );
+    
+    float cosAlpha = (length_v0v1+length_v1v2-length_v2v0)/( 2*length_v0v1_sqrt*length_v1v2_sqrt );
+
+    return acos( cosAlpha );
+  }
+  
 public:
   void setMesh( MeshCore* _mesh ) {
     pMesh = _mesh;
   }
   void calculateEdgeCotValues(  ) {
-    Vertex_handle v0,v1,v2;
+    Vertex_handle v0,v1,v2;    
+    float alpha;
+    
     for (Halfedge_iterator pHalfedge = pMesh->halfedges_begin(  );
          pHalfedge != pMesh->halfedges_end(  ); pHalfedge++)
     {
       v0 = pHalfedge->vertex(  );
       v1 = pHalfedge->next(  )->vertex(  );
       v2 = pHalfedge->next(  )->next(  )->vertex(  );
-      //calculate <v0v1v2
-      
-      
-      
-    }
-    
+
+      alpha = _calculateAngle( v0, v1, v2 );
+
+      float value = 1/tan( alpha );
+      put( edgeCotValues, pHalfedge, value );
+    }    
   }
 
   void calculateVoronoiAreas(  ) {
