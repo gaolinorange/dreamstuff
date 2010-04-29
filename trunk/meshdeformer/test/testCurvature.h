f6f903d9f9c0009a50631ca7c853cc022f0fd5d3
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2010-04-29 01:52:43 +0000

    add curvature voronoi test cases

--- trunk/meshdeformer/test/testCurvature.h
+++ trunk/meshdeformer/test/testCurvature.h
@@ -28,8 +28,8 @@ class CurvatureTest : public ::testing::Test
   virtual void SetUp(  ) {
     mesh_ = new MeshCore(  );
     Halfedge_handle h = mesh_->make_tetrahedron( Point_3( 0,0,0 ),
-                                                 Point_3( 0,0,1 ),
                                                  Point_3( 0,1,0 ),
+                                                 Point_3( 0,0,1 ),
                                                  Point_3( 1,0,0 )
                                                  );
     mesh_->set_indices( );
@@ -64,6 +64,7 @@ TEST_F( CurvatureTest, testInit ) {
 }
 
 TEST_F( CurvatureTest, testPrintMesh ) {
+  std::size_t i = 0;
   for( Vertex_iterator pVertex = mesh_->vertices_begin(  );
        pVertex != mesh_->vertices_end(  ); pVertex++) {
     printf( "id: %ld: ( %.2f, %.2f, %.2f )\n",
@@ -71,46 +72,60 @@ TEST_F( CurvatureTest, testPrintMesh ) {
             pVertex->point(  ).x(  ),
             pVertex->point(  ).y(  ),
             pVertex->point(  ).z(  ));
+
+    //using getVertexHandle
+    Vertex_handle ref_v_handle = mesh_->get_vertex_handle( i );
+    ASSERT_FLOAT_EQ( ref_v_handle->point().x(),pVertex->point(  ).x(  ) );
+    ASSERT_FLOAT_EQ( ref_v_handle->point(  ).y(  ), pVertex->point(  ).y(  ) );
+    ASSERT_FLOAT_EQ( ref_v_handle->point(  ).z(  ), pVertex->point(  ).z(  ) );
+    
+    i++;
   }
 }
 
 TEST_F( CurvatureTest, printPrecalculateCotValues ) {
-  printf( "Cot45Value: %.5f", cot45Value );
-  printf( "Cot60Value: %.5f", cot60Value );
-  printf( "Cot90Value: %.5f", cot90Value );
+  printf( "Cot45Value: %.5f\n", cot45Value );
+  EXPECT_FLOAT_EQ( 1, cot45Value );
+  printf( "Cot60Value: %.5f\n", cot60Value );
+  EXPECT_FLOAT_EQ( sqrt( 3.0 )/3, cot60Value );
+  printf( "Cot90Value: %.5f\n", cot90Value );
+  EXPECT_NEAR( 0.0, cot90Value, epislon );
 }
 
-TEST_F( CurvatureTest, testAngle ) {
+TEST_F( CurvatureTest, testCalculateAngle ) {
   Halfedge_handle h = mesh_->halfedges_begin(  );
   Vertex_handle v0 = h->vertex(  );
   Vertex_handle v1 = h->next(  )->vertex(  );
   Vertex_handle v2 = h->next(  )->next(  )->vertex(  );
-  
-  float value = curvature_->_calculateAngle( v0, v1, v2 )  ;
-  printf( "the angle value is : %.2f",value );
+
+  //debugging helper: for judging which triangle you're using
+/*   printf( "v0: ( %.2f,%.2f,%.2f )\n",v0->point(  ).x(  ),v0->point(  ).y(  ),v0->point(  ).z(  ) ); */
+/*   printf( "V1: ( %.2f, %.2f,%.2f) \n",v1->point(  ).x(  ),v1->point(  ).y(  ),v1->point(  ).z(  ) ); */
+/* printf ("v2: ( %.2f, %.2f, %.2f )\n",v2->point(  ).x(  ),v2->point(  ).y(  ),v2->point(  ).z(  )); */
+  float value = curvature_->calculateAngle( v0, v1, v2 )  ;
+  printf( "the angle value is : %.2f\n",value );
+  float expected_angle = ( 45.0/180.0 ) * CGAL_PI;
+  EXPECT_NEAR( expected_angle, value, epislon );
 }
 
-TEST_F( CurvatureTest, testHalfedgeCotValues ) {
-  curvature_->calculateEdgeCotValues(  )  ;
-
-  float value;
-  for (Halfedge_iterator pHalfedge = mesh_->halfedges_begin(  );
-       pHalfedge != mesh_->halfedges_end(  ); ++pHalfedge)
-  {
-    value = curvature_->getHalfedgeCotValue( pHalfedge );
-    printf( "Halfedge %d's cotvalue: %.2f\n", pHalfedge->id(  ), value );
-  }
+TEST_F( CurvatureTest, testCalculateVecLength ) {
+  Halfedge_handle h = mesh_->halfedges_begin(  );
+  Vertex_handle v0 = h->vertex(  );
+  Vertex_handle v1 = h->next(  )->vertex(  );
+  float length = curvature_->calculateVecLength( v0,v1 );
+  EXPECT_FLOAT_EQ( 1.0, length );
 }
 
 TEST_F( CurvatureTest, testEdgeCotValues ) {
   curvature_->calculateEdgeCotValues(  );
-  //TODO: add some assertions
+
   Vertex_handle  v0,v1;
   Vector_3 vec;
   float length;
   float expected;
   float actual;
-  
+
+  //Checking all edge's corresponding cot_sum_values  
   for (Halfedge_iterator pHalfedge = mesh_->halfedges_begin( );
        pHalfedge != mesh_->halfedges_end( ); pHalfedge++)
   {
@@ -137,8 +152,24 @@ TEST_F( CurvatureTest, testEdgeCotValues ) {
 TEST_F( CurvatureTest, testVoronoiAreas ) {
   curvature_->calculateVoronoiAreas(  );
   //TODO: add some assertions
-  
-  
+  for (Vertex_iterator pVertex =  mesh_->vertices_begin(  );
+       pVertex != mesh_->vertices_end(  ); ++pVertex)
+  {
+    //two kind of test_cases
+    //1. around ( 0,0,0 )
+    if( fabs(pVertex->point(  ).x(  )-0.0) < epislon &&
+        fabs( pVertex->point(  ).y(  )-0.0 ) < epislon &&
+        fabs( pVertex->point(  ).z(  )-0.0 ) < epislon ) {
+      float one_voronoi = 1.0/8 * ( 1+1 );
+      float expect_voronoi_area = 3 * one_voronoi;
+      EXPECT_FLOAT_EQ( expect_voronoi_area, curvature_->getVertexVoronoiArea( pVertex ) );
+    } else {     //2. around( ?,1,? )
+      float angle45_voronoi = 1.0/8 * ( 1 );
+      float angle60_voronoi = 1.0/8 * ( 2*sqrt( 3.0 )/3 + 2*sqrt( 3.0 )/3 );
+      float expect_voronoi_area = 2*angle45_voronoi + angle60_voronoi;
+      EXPECT_FLOAT_EQ( expect_voronoi_area, curvature_->getVertexVoronoiArea( pVertex ) );
+    }    
+  }  
 }
 
 TEST_F( CurvatureTest, testMeanCurvature ) {
--- trunk/meshdeformer/test/testMain.cpp
+++ trunk/meshdeformer/test/testMain.cpp
@@ -8,7 +8,7 @@ int main(int argc,char** argv)
   
   ::testing::InitGoogleTest(&argc,argv);
 
-  RUN_ALL_TESTS();
+  return RUN_ALL_TESTS();
   
   return app.exec(  );
 }
