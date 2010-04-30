1d1ff56d98007d1a77625dfe95280dc4ff72073b
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2010-04-30 09:36:15 +0000

    bugfix: add GLUT support for MeshCore, which is the fundamental of plugin-eds

--- meshdeformer/imesh/Common/globals.hpp
+++ meshdeformer/imesh/Common/globals.hpp
@@ -8,42 +8,7 @@ extern bool RUNNING_AT_TEST_MODE;
 
 const int MAIN_VERSION = 0 ;
 const int MAJOR_VERSION = 0;
-const int MINOR_VERSION = 1;
-
-/*
-//Some facility for debugging, defined in global mode
-class AppSettings
-{
-public:
-	static AppSettings getSingleton()
-	{
-		return settings;
-	}
-	static AppSettings* getSingletonPtr()
-	{
-		return &settings;
-	}
-public:
-	AppSettings()
-	{
-		RUNNING_AT_TEST_MODE = false;
-	}
-	~AppSettings()
-	{
-	}
-
-
-public:
-	void setRunningAtTestMode(bool flag)
-	{
-		RUNNING_AT_TEST_MODE = flag;
-	}
-private:
-	bool RUNNING_AT_TEST_MODE;
-private:
-	static AppSettings settings;
-};
-*/
+const int MINOR_VERSION = 2;
 
 
 #endif
