bcb3343030c5d8c87376466521292ae3e9f9e4a5
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2010-05-04 02:56:56 +0000

    bugfix&enhancemnet: deformation graph rearrange

--- meshdeformer/test/testDeformationGraph.h
+++ meshdeformer/test/testDeformationGraph.h
@@ -26,12 +26,39 @@ class DeformationGraphTest : public MeshCoreTest
    virtual void SetUp(  ) {
      MeshCoreTest::SetUp(  );
      deformation_graph_ = new DeformationGraph(  );
+     setupTheTestData(  );
    }
    virtual void TearDown(  ) {
      delete deformation_graph_;
      MeshCoreTest::TearDown(  );
    }
    DeformationGraph* deformation_graph_;
+private:
+   void setupTheTestData(  ) {
+     DeformationGraphNode node1;
+     node1.set_id( 0 );
+     node1.position_[ 0 ] = node1.position_[ 1 ] = node1.position_[ 2 ] = 0.0;
+     node1.insert_neighbor_node( 1 );
+     node1.set_parent_deformation_graph( deformation_graph_ );
+     deformation_graph_->nodes_.push_back( node1 );
+
+     DeformationGraphNode node2( 1 );
+     node2.position_[ 0 ] = node2.position_[ 1 ] = 0.0;
+     node2.position_[ 2 ] = 1.0f;
+     node2.insert_neighbor_node( 0 );
+     node2.set_parent_deformation_graph( deformation_graph_ );
+     deformation_graph_->nodes_.push_back( node2 );
+
+
+     DeformationGraphNode node3( 2 );
+     node3.position_[ 0 ] = 0.0;
+     node3.position_[ 1 ] = 1.0;
+     node3.position_[ 2 ] = 0.0;
+     node3.insert_neighbor_node( 0 );
+     node3.insert_neighbor_node( 1 );
+     node3.set_parent_deformation_graph( deformation_graph_ );
+     deformation_graph_->nodes_.push_back( node3 );     
+   }
  };
 
 TEST_F( DeformationGraphTest, testInit ) {
@@ -43,6 +70,22 @@ TEST_F( DeformationGraphTest, testConstruct ) {
   deformation_graph_->construct( pMesh, 4 )  ;
 }
 
+TEST_F( DeformationGraphTest, printNodes ) {
+  vector<DeformationGraphNode>::iterator pNode;
+  for( pNode = deformation_graph_->nodes_.begin(  );
+       pNode != deformation_graph_->nodes_.end(  ); pNode++ ) {
+    pNode->print(  );
+    printf( "\n" );
+  }
+}
+
+TEST_F( DeformationGraphTest, testRenderConnection ) {
+  vector<DeformationGraphNode>::iterator pNode;
+  for (pNode = deformation_graph_->nodes_.begin(  );
+       pNode != deformation_graph_->nodes_.end(); ++pNode) {
+    pNode->renderConnection(  );    
+  }
+}
 
 
 #endif /* _TESTDEFORMATIONGRAPH_H_ */
