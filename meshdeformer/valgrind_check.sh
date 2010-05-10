066b2cf9bdb8ac15241de78dae1169bb5d5a27bf
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2010-05-10 10:10:00 +0000

    add gmock learning note

--- meshdeformer/valgrind_check.sh
+++ meshdeformer/valgrind_check.sh
@@ -1,1 +1,2 @@
-valgrind --log-file=valgrind.log --leak-check=full build/bin/meshdeformer
+valgrind --log-file=test_valgrind.log --leak-check=full -v build/bin/test
+valgrind --log-file=imesh_valgrind.log --leak-check=full -v build/bin/imesh
