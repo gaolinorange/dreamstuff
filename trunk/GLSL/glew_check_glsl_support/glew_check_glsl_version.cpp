c60ef665f6e49bb754da5da1da358d10b0bb2258
lai.jingwen2008 <lai.jingwen2008@21fd095a-c762-11de-9e96-3193d784f102>
2010-06-09 07:29:39 +0000

    add glew test project

---
+++ trunk/GLSL/glew_check_glsl_support/glew_check_glsl_version.cpp
@@ -0,0 +1,38 @@
+// glew_check_glsl_version.cpp --- check the glsl ability using glew
+// ref: www.lighthouse3d.com/opengl/glsl/index.php?ogloverview
+// copyright (c) 2010 dreamway
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by the Free Software Foundation;
+//  Feel free to contact dreamway(jingwenlai@163.com)
+// 
+// website: cg.xplore.cn
+// 
+// 
+
+// Commentary: 
+// 
+//
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+int main(int argc, char *argv[])
+{
+  glutInit( &argc, argv );
+  glewInit(  );
+  if( glewIsSupported( "GL_VERSION_2_0" ) )
+    printf( "Ready for OpenGL 2.0\n" );
+  else {
+    printf( "OpenGL 2.0 not supported\n" );
+    return -1;
+  }
+  
+  glutMainLoop(  );
+
+  return 0;
+}
+
+//  
+// 
+// glew_check_glsl_version.cpp ends here
